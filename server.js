f66be71d9b51b002aee3e1c16a887347933e70fd
joseph reis <josephreis646@gmail.com>
2023-04-03 15:31:01 -0400

    Set up server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user-routes');
+const thoughtRoutes = require('./routes/thought-routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
