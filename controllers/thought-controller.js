d863b2b3c55726178a146b07a97812b4672ec557
joseph reis <josephreis646@gmail.com>
2023-04-04 13:10:27 -0400

    update thought function

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -43,3 +43,18 @@ const thoughtController = {
       })
       .catch((err) => res.status(500).json(err));
   },
+
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
