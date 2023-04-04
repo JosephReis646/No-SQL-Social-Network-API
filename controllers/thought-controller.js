4a55c139653f418dbd278d345fd9d15af05d319d
joseph reis <josephreis646@gmail.com>
2023-04-04 13:09:23 -0400

    get thought by ID function

---
+++ controllers/thought-controller.js
@@ -0,0 +1,26 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
