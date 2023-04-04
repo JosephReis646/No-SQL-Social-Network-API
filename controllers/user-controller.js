259c7ff555e301be9170301d1a4f2f9967632247
joseph reis <josephreis646@gmail.com>
2023-04-04 13:05:33 -0400

    update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -40,3 +40,18 @@ const userController = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
